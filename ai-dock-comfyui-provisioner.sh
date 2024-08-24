c40d932ab7b6f433e852510acb20321e964b7105
Jordan Coult <jordan@auterion.com>
2024-08-24 08:20:32 -0500

    manually add realvisxlv4.0_.v30InpaintBakedVAE

--- ai-dock-comfyui-provisioner.sh
+++ ai-dock-comfyui-provisioner.sh
@@ -64,6 +64,7 @@ NODES=(
 )
 
 CHECKPOINT_MODELS=(
+    "https://huggingface.co/JordanCo/realvisxlV40_v30InpaintBakedvae/resolve/main/realvisxlV40_v30InpaintBakedvae.safetensors"
     #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
